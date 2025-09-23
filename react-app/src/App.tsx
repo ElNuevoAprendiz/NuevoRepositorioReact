bb957dc1b5a1d46886873891843dcfc103f112ee
ElNuevoAprendiz! <profedanieljuan@gmail.com>
2025-09-23 10:23:34 -0300

    Se agrega el componente List, se usa la propiedad children, y el tipo de dato ReactNode

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,14 @@
 import Card, {CardBody} from "./components/Card";
+import List from "./components/List";
 
 function App() {
-  return <Card>
-    <CardBody title="El titulo" text="El texto del cuerpo"/></Card>;
+  return (
+    <Card>
+      <CardBody title="El titulo" text="El texto del cuerpo"/>
+      <List/>  
+    </Card>
+    
+  )
     }
 
 export default App;
