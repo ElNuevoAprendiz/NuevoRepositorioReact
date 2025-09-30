df4fc7a5a0ce09b2ac79da5660aa8156071db9cb
ElNuevoAprendiz! <profedanieljuan@gmail.com>
2025-09-30 09:19:48 -0300

    Se usan el manejador de eventos oncClick y la función flecha para definir la función que se ejecutará al hacer clic en el botón.
    También se agrega un identificador único (key) a cada elemento de la lista
    usando el índice del elemento en el array. Esto es importante para que React pueda
    optimizar la renderización de listas.

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -11,11 +11,18 @@ function List({ data }: Props) {
     
         <ul className="list-group">
           {data.map((elemento) => (
-          <li className="list-group-item">{elemento}</li>
-        ))}
+          <li onClick={()=>console.log(elemento)} key={elemento} className="list-group-item">{elemento}</li>
+          ))}
         </ul>
-    
+        
   )
+  {/*Como cada elemento de la lista es unico, lo usamos key como propiedad que va a identificar
+    de manera única a cada elemento de la lista, y para eso le agregamos en cada iteracción
+    a key el nombre que devuelve el parametro {elemento}*/}
+  {/*El onClick es un manejador de eventos que se dispara cuando el usuario hace click en el elemento
+     de la lista. A diferencia de la propiedad onclick de cada elemento html, este se escribe con la
+     C en mayúscula, y en este caso usamos un arrow function para reaccionar al evento*/}
+
 }
 
 export default List
