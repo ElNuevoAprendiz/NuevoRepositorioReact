cecd5a04ba9a6c3f6355b81abb3eaa00000348c5
Dany <profedanieljuan@gmail.com>
2025-10-01 20:12:52 -0300

    Merge nuevo repositorio con historiales no relacionados en la net

--- react-app/src/components/Card.tsx
+++ react-app/src/components/Card.tsx
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
-interface Props {
-    body: string 
-    
-}
-
-function Card(props: Props) {
-    const { body } = props; {/* Es una desestruracion de las propiedades pasadas al objeto, que hemos llamdo props */}
-=======
 import { ReactNode } from "react";
 
-
 interface Props {
-    children: ReactNode; 
+  children: ReactNode;
 }
-{/* ReactNode es un tipo especial de dato que puede contener cualquier cosa que React pueda renderizar, como texto, elementos JSX, componentes, etc. */}
+{
+  /* ReactNode es un tipo especial de dato que puede contener cualquier cosa que React pueda renderizar, como texto, elementos JSX, componentes, etc. */
+}
 
 function Card(props: Props) {
-    const {children} = props; {/* Es una desestruracion de las propiedades pasadas al objeto, que hemos llamdo props */}
->>>>>>> df4fc7a5a0ce09b2ac79da5660aa8156071db9cb
+  const { children } = props;
+  {
+    /* Es una desestruracion de las propiedades pasadas al objeto, que hemos llamdo props */
+  }
   return (
     <div
       className="card"
@@ -25,45 +19,23 @@ function Card(props: Props) {
         width: "350px",
       }}
     >
-<<<<<<< HEAD
-      <div className="card-body">{body}</div> {/*recordar que body es una propiedad del objeto props*/}
-=======
-      <div className="card-body">{children}</div> {/*recordar que children es una propiedad del objeto props y contiene lo que vamos a dibujar*/}
->>>>>>> df4fc7a5a0ce09b2ac79da5660aa8156071db9cb
+      <div className="card-body">{children}</div>{" "}
+      {/*recordar que children es una propiedad del objeto props y contiene lo que vamos a dibujar*/}
     </div>
   );
 }
 
-<<<<<<< HEAD
-export function CardBody() {
-  return (
-    <>
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card’s content.
-      </p>
-      <a href="#" className="btn btn-primary">
-        Go somewhere
-      </a>
-=======
 interface CardBodyProps {
-    title: string;
-    text?: string;
-
+  title: string;
+  text?: string;
 }
 export function CardBody(props: CardBodyProps) {
-  
-  const {title, text} = props;
-  
+  const { title, text } = props;
+
   return (
     <>
       <h5 className="card-title">{title}</h5>
-      <p className="card-text">
-        {text}
-      </p>
-      
->>>>>>> df4fc7a5a0ce09b2ac79da5660aa8156071db9cb
+      <p className="card-text">{text}</p>
     </>
   );
 }
